02b0cadb11e79466b6c8e13405019f8f96cd3d73
oyrad <dario.susanj2@gmail.com>
2024-06-16 14:37:36 +0200

    add prettier and video

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        "svemirko-green": "#00B47C",
-        "svemirko-yellow": "#FFAE2D",
-        "svemirko-red": "#E4024E",
-        "svemirko-blue": "#2A96BF",
-        "hero-background": "#F5F6EE",
+        'svemirko-green': '#00B47C',
+        'svemirko-yellow': '#FFAE2D',
+        'svemirko-red': '#E4024E',
+        'svemirko-blue': '#2A96BF',
+        'hero-background': '#F5F6EE',
+        karanfili: '#ffdc4b',
       },
     },
   },
